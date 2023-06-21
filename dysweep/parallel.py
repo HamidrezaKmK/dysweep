acfe37685dbe947f9a068e49d800013fafdf8726
HamidrezaKmK <hamidrezakamkari@gmail.com>
2023-06-21 18:00:21 -0400

    add metric arguments and debug multi-resume

--- dysweep/parallel.py
+++ dysweep/parallel.py
@@ -16,7 +16,7 @@ SPLIT = '-'
 
 @dataclass
 class ResumableSweepConfig:
-    default_root_dir: th.Union[Path, str]
+    default_root_dir: th.Optional[th.Union[Path, str]] = None
     custom_checkpoint_dir: th.Optional[th.Union[Path, str]] = None
     sweep_configuration: th.Optional[dict] = None
     base_config: th.Optional[dict] = None
@@ -25,7 +25,7 @@ class ResumableSweepConfig:
     project: th.Optional[str] = None
     count: th.Optional[int] = None
     resume: bool = False
-    rerun_id: th.Optional[int] = None
+    rerun_id: th.Optional[str] = None
     run_name: th.Optional[str] = None
     sweep_id: th.Optional[th.Union[int, str]] = None
     #
@@ -39,7 +39,12 @@ class ResumableSweepConfig:
 def check_non_empty(checkpoint_dir):
     all_subdirs = [d for d in checkpoint_dir.iterdir() if d.is_dir()]
     return len(all_subdirs) > 0
-
+ 
+def get_max(all_subdirs):
+    if len(all_subdirs) == 0:
+        return 0
+    identifiers = [int(d.name.split(SPLIT)[0]) for d in all_subdirs]
+    return max(identifiers)
 
 def dysweep_run_resume(
     conf: th.Optional[ResumableSweepConfig] = None,
@@ -51,19 +56,19 @@ def dysweep_run_resume(
     entity: th.Optional[str] = None,
     project: th.Optional[str] = None,
     count: th.Optional[int] = None,
-    resume: bool = False,
-    rerun_id: th.Optional[int] = None,
+    resume: th.Optional[bool] = None,
+    rerun_id: th.Optional[str] = None,
     run_name: th.Optional[str] = None,
     sweep_id: th.Optional[th.Union[int, str]] = None,
-    use_lightning_logger: bool = False, 
-    method: th.Optional[str] = 'grid',
-    metric: th.Optional[str] = 'dysweep_default',
-    goal: th.Optional[str] = 'minimize',
-    sweep_name: th.Optional[str] = 'dysweep',
+    use_lightning_logger: th.Optional[bool] = None,
+    method: th.Optional[str] = None,
+    metric: th.Optional[str] = None,
+    goal: th.Optional[str] = None,
+    sweep_name: th.Optional[str] = None,
 ):
     """
     This is a multi-purpose function that does either one of the following functionalities:
-    
+
     1. Creates a new sweep:
         This happens when sweep_id is not given. In which case, it will
         create a new sweep object according to the project and entity
@@ -79,26 +84,26 @@ def dysweep_run_resume(
         by either cluster preemption or by there being a specific bug that creates
         an exception in the code or kills it. In this case, under the checkpoint_dir
         (you can also specify it using `custom_checkpoint_dir`) you can see a summary
-        of all the checkpoints. 
-        
+        of all the checkpoints.
+
     4. Re-runs one of the configurations:
-        In this case, a specific run_id is given. This run_id should have been ran on the 
+        In this case, a specific run_id is given. This run_id should have been ran on the
         machine where re-running occurs. Now the function will retrieve the configuration
         that the run was previously executed with, retrieves any checkpoints if existant
         and `resume` is set to `True`.
-    
+
     Instead of defining every single input to this function, you may also pass in a ResumableSweepConfig
-    object instead. We included this feature because the intended usage of this package is coupled with 
+    object instead. We included this feature because the intended usage of this package is coupled with
     `jsonargparse` where all the configurations are written in YAML format. (Check the examples section in the
     document).
-    
+
     Args:
-        conf: 
+        conf:
             The entire configuration given as a ResumableSweepConfig object.
         function:
             This is a function that *YOU* have implemented and has the following signature:
             1. function(config, checkpoint_dir)
-                Takes in the configuration and the checkpoint_dir associated with the run. 
+                Takes in the configuration and the checkpoint_dir associated with the run.
                 You can store anything you want in the `checkpoint_dir` and then implement your
                 own checkpoint retrieval procedure so that `function` can resume too.
             2. function(config, logger, checkpoint_dir)
@@ -109,7 +114,7 @@ def dysweep_run_resume(
         default_root_dir:
             This is a required argument, you can specify a root directory in which all the checkpoint logs
             and everything related to the library is being stored.
-        custom_checkpoint_dir: 
+        custom_checkpoint_dir:
             For checkpoints in specific, you can also define a custom_checkpoint_dir under the root_dir where
             the checkpoints will be stored.
         sweep_configuration: dict
@@ -121,11 +126,11 @@ def dysweep_run_resume(
             The entity where the sweep and runs are defined.
         project: str
             The project in which the sweep is supposed to reside in or be placed in if it is
-            being instantiated.   
+            being instantiated.
         count: optional(int)
             Count only works when calling the agent to get a new set of configurations
             from the sweep server. For example, with count=2, the function will be run
-            only on 2 new configurations. 
+            only on 2 new configurations.
             If you have a machine that stays alive for a long time, set the `count` value
             to be large; however, if you have a lot of machines that stay alive for not too
             long, it would be better to set the `count=1` and run a lot of these machines.
@@ -134,7 +139,7 @@ def dysweep_run_resume(
             it will also find the checkpoint_dir associated with that identifier and pass it to
             function. If rerun_id is not given, it will search for any runs in the `checkpoint_dir`
             that has failed and pass that to the function.
-        rerun_id: optional(int)
+        rerun_id: optional(str)
             When re-running a specific run that you know it has failed, you can use this argument to specify.
         run_name: optional(str)
             If you want to set a specific names for your runs in the wandb UI, then you can specify here.
@@ -151,7 +156,7 @@ def dysweep_run_resume(
         goal: optional(str)
             The goal used for the sweep.
         use_lightning_logger: optional(bool) = False
-            When set to True, it will pass an additional argument `logger` to `function` that contains the 
+            When set to True, it will pass an additional argument `logger` to `function` that contains the
             lightning logger wrapper.
     Returns:
         It returns either one of the following:
@@ -159,12 +164,8 @@ def dysweep_run_resume(
         2. Returns the output of the function if the configuration only tries to run a single experiment.
         3. Otherwise, it will return None.
     """
-    if project is None:
-        raise ValueError("project should be given to the dysweep run and resume.")
-    if function is None and sweep_id is not None:
-        raise ValueError("function should be given to the dysweep run and resume when sweep_id is given.")
     # if configuration is not given, then create it from the
-    # input arguments 
+    # input arguments
     if conf is None:
         conf = ResumableSweepConfig(
             default_root_dir=default_root_dir,
@@ -178,12 +179,53 @@ def dysweep_run_resume(
             rerun_id=rerun_id,
             run_name=run_name,
             sweep_id=sweep_id,
-            use_lightning_logger=use_lightning_logger, 
+            use_lightning_logger=use_lightning_logger,
             method=method,
             metric=metric,
             sweep_name=sweep_name,
             goal=goal,
         )
+    else:
+        # if for any argument x, the value of x is not the default value
+        # then overwrite conf.x with x
+        if default_root_dir is not None:
+            conf.default_root_dir = default_root_dir
+        if custom_checkpoint_dir is not None:
+            conf.custom_checkpoint_dir = custom_checkpoint_dir
+        if sweep_configuration is not None:
+            conf.sweep_configuration = sweep_configuration
+        if base_config is not None:
+            conf.base_config = base_config
+        if entity is not None:
+            conf.entity = entity
+        if project is not None:
+            conf.project = project
+        if count is not None:
+            conf.count = count
+        if resume is not None:
+            conf.resume = resume
+        if rerun_id is not None:
+            conf.rerun_id = rerun_id
+        if run_name is not None:
+            conf.run_name = run_name
+        if sweep_id is not None:
+            conf.sweep_id = sweep_id
+        if use_lightning_logger is not None:
+            conf.use_lightning_logger = use_lightning_logger
+        if method is not None:
+            conf.method = method
+        if metric is not None:
+            conf.metric = metric
+        if sweep_name is not None:
+            conf.sweep_name = sweep_name
+        if goal is not None:
+            conf.goal = goal
+        
+        
+    if conf.project is None:
+        raise ValueError("project should be given to the dysweep run and resume.")
+    if function is None and sweep_id is not None:
+        raise ValueError("function should be given to the dysweep run and resume when sweep_id is given.")
 
     if conf.sweep_id is not None:
         if conf.default_root_dir is None:
@@ -239,7 +281,7 @@ def dysweep_run_resume(
                         experiment_id = SPLIT.join(dir_name.split(SPLIT)[1:])
                     else:
                         experiment_id = conf.rerun_id
-                    
+
                     # Using experiment_id, either get the actual configuration
                     # from the running directory or from the stored json file
                     old_dir_name = None
@@ -252,17 +294,17 @@ def dysweep_run_resume(
                             if id_ == experiment_id:
                                 old_dir_name = d.name
                                 config_dir = checkpoint_dir / d.name / "sweep_config.json"
-                    
+
                     if not os.path.exists(config_dir):
                         raise FileNotFoundError(f"{config_dir} not found! Make sure the rerun_id is actually ran before.")
-                    
-                    new_dir_name = f"{len(all_subdirs) + 1}{SPLIT}{experiment_id}"
-                    
+
+                    new_dir_name = f"{get_max(all_subdirs) + 1}{SPLIT}{experiment_id}"
+
                     # Load the configuration that was already used for running
                     # the function before.
                     with open(config_dir, "r") as f:
                         sweep_config = json.load(f)
-                        
+
                     if old_dir_name is not None:
                         # Change the name of the directory by pushing it to the end of the queue
                         shutil.copytree(checkpoint_dir / old_dir_name,
@@ -271,8 +313,8 @@ def dysweep_run_resume(
 
                     # create a new checkpoint directory for the inner function
                     new_checkpoint_dir = checkpoint_dir / new_dir_name
-                    
-                    # Retrieve the logger  
+
+                    # Retrieve the logger
                     if conf.use_lightning_logger:
                         # Create a WandbLogger with the experiment_id
                         from lightning.pytorch.loggers import WandbLogger
@@ -313,10 +355,10 @@ def dysweep_run_resume(
                             name=run_name,
                         )
                         experiment_id = run_.id
-                        
+
                         sweep_config = hierarchical_config(wandb.config)
 
-                    new_dir_name = f"{len(all_subdirs)+1}{SPLIT}{experiment_id}"
+                    new_dir_name = f"{get_max(all_subdirs)+1}{SPLIT}{experiment_id}"
 
                     os.makedirs(checkpoint_dir / new_dir_name)
 
@@ -342,7 +384,7 @@ def dysweep_run_resume(
                 # check that the function has two parameters
                 if len(params) != 3 or list(params.keys())[0] != "config" or list(params.keys())[1] != "logger" or list(params.keys())[2] != "checkpoint_dir":
                     raise ValueError(
-                        "the run function should have the exact following parameters in order: (config, logger, checkpoint_dir)")
+                        "the function passed to `dysweep_run_resume` should have the exact following parameters in order: (config, logger, checkpoint_dir)")
                 try:
                     ret = function(sweep_config, logger, new_checkpoint_dir)
                 except Exception as e:
@@ -363,7 +405,7 @@ def dysweep_run_resume(
                 # check that the function has two parameters
                 if len(params) != 2 or list(params.keys())[0] != "config" or list(params.keys())[1] != "checkpoint_dir":
                     raise ValueError(
-                        "the run function should have the exact following parameters in order: (config, checkpoint_dir)")
+                        "the function passed to `dysweep_run_resume` should have the exact following parameters in order: (config, checkpoint_dir)")
                 try:
                     ret = function(sweep_config, new_checkpoint_dir)
                 except Exception as e:
@@ -380,10 +422,10 @@ def dysweep_run_resume(
             shutil.rmtree(new_checkpoint_dir)
             # finish the wandb run so that later .init calls can resume different ones
             wandb.finish()
-            
+
             return ret
-        
-        
+
+
         if conf.resume and not conf.rerun_id:
             # In this case, we will sequantially resume
             # any run that is remaining with the limit of `count`
@@ -398,7 +440,7 @@ def dysweep_run_resume(
                             target=modified_function)
                         modified_function_thread.start()
                         modified_function_thread.join()
-                        
+
                     else:
                         break
             else:
@@ -425,7 +467,7 @@ def dysweep_run_resume(
             }
 
         try:
-            sweep_id = sweep(conf.base_config, conf.sweep_configuration, 
+            sweep_id = sweep(conf.base_config, conf.sweep_configuration,
                              entity=conf.entity, project=conf.project)
         except Exception as e:
             print("Exception at creation of sweep:")
