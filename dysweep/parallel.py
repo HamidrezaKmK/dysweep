7637d09c73a9a2610d1eee4481fbc939cb22ca47
Hamidreza Kamkari <hamidrezakamkari@gmail.com>
2023-07-28 20:54:25 -0500

    Update parallel.py

--- dysweep/parallel.py
+++ dysweep/parallel.py
@@ -469,11 +469,9 @@ def dysweep_run_resume(
             # running.
             shutil.copyfile(new_checkpoint_dir / "sweep_config.json",
                             checkpoint_dir / f"{experiment_id}-config.json")
-            if conf.delete_checkpoints:
-                shutil.rmtree(new_checkpoint_dir)
-            else:
-                # copy everything from new_checkpoint_dir to checkpoint_dir
+            if not conf.delete_checkpoints:
                 shutil.copytree(new_checkpoint_dir, checkpoint_dir / "{experiment_id}-final")
+            shutil.rmtree(new_checkpoint_dir)
                 
             # finish the wandb run so that later .init calls can resume different ones
             wandb.finish()
