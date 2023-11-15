40bcd0ec1792133b7e0b79f51d43a960e59cf678
Hamidreza Kamkari <hamidrezakamkari@gmail.com>
2023-11-15 17:40:52 -0500

    Update __init__.py
    version 0.1.6

--- dysweep/__init__.py
+++ dysweep/__init__.py
@@ -2,4 +2,4 @@ from .parallel import dysweep_run_resume, ResumableSweepConfig
 from .wandbX import hierarchical_config
 from .helper import parse_dictionary_onto_dataclass
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
